a5b58a5a464382db46830ae747a09a79f3cae6ec
z1qnezt <misha232dog@gmail.com>
2024-03-07 15:00:24 +0300

    Hello_world+username

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    cout << "Hello world";
+    string name = "";
+    cin >> name;
+    cout << "Hello world from " << name;
     return 0;
 }
